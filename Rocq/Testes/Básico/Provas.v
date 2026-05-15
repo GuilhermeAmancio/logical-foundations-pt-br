a3c5ab86a36c9b98ab25a3ba928c70196d6ad964
GuilhermeAmancio <guilherme.amancio@dcomp.ufs.br>
2026-05-15 01:24:56 -0300

    Começando exercícios do fim do capítulo

--- Rocq/Testes/Básico/Provas.v
+++ Rocq/Testes/Básico/Provas.v
@@ -212,5 +212,16 @@ Proof.
 Qed.
    
 
+(* Exercício *)
+Theorem zero_nbeq_plus_1 : forall n : nat,
+   0 =? (n + 1) = false.
+
+Proof.
+   intros n. destruct n as [|n'] eqn: E.
+   - simpl. reflexivity.
+   - simpl. reflexivity.   Qed.
+   
+ 
+
 
 
